170df3d3d1268315c1a6eab39d96785ac2040068
dinhtron <80462269+dinhtron@users.noreply.github.com>
2023-12-01 21:32:50 +0700

    Your commit message here

---
+++ client1.py
@@ -0,0 +1,60 @@
+import socket
+import json
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(("127.0.0.1", 8080))
+
+    # Nhập thông tin đăng nhập
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    # Gửi thông tin đăng nhập đến server
+    client.send(username.encode())
+    client.send(password.encode())
+
+    # Nhận kết quả xác thực
+    auth_result = client.recv(1024).decode()
+    print(auth_result)
+
+    if auth_result == "truse":
+        while True:
+            # Hiển thị menu lựa chọn
+            print("1. Datsan")
+            print("2. Lichsu")
+            print("3. Thoat")
+
+            choice = input("Enter your choice: ")
+
+            # Gửi lựa chọn đến server
+            client.send(choice.encode())
+
+            if choice == "thoat":
+                break
+            elif choice == "datsan":
+                # Nhận và hiển thị danh sách sân trống
+                data_received = client.recv(1024).decode()
+                san_trong_info = json.loads(data_received)
+                print("Danh sách sân trống:", san_trong_info["san_trong"])
+
+                # Cho phép client chọn sân và giờ để đặt
+                chosen_san = input("Chon san de dat: ")
+                chosen_gio_dat = input("Nhap gio dat: ")
+                data_to_send = {"chosen_san": chosen_san, "chosen_gio_dat": chosen_gio_dat}
+                client.send(json.dumps(data_to_send).encode())
+
+                # Nhận kết quả đặt sân từ server
+                result = client.recv(1024).decode()
+                print(result)
+            elif choice == "lichsu":
+                # Nhận và hiển thị danh sách sân đã đặt
+                data_received = client.recv(1024).decode()
+                san_da_dat_info = json.loads(data_received)
+                print("Danh sách sân đã đặt:", san_da_dat_info["san_da_dat"])
+            else:
+                print("Invalid choice")
+
+    client.close()
+
+if __name__ == "__main__":
+    main()
---
+++ server1.py
@@ -0,0 +1,81 @@
+import socket
+import json
+
+# Danh sách tài khoản
+users = {"trong": "123", "user2": "pass2", "user3": "pass3"}
+
+# Tập hợp chứa thông tin về các sân đã đặt
+san_da_dat = set()
+
+def handle_client(client_socket):
+    global san_da_dat
+
+    # Kiểm tra đăng nhập
+    username = client_socket.recv(1024).decode()
+    password = client_socket.recv(1024).decode()
+
+    if username in users and users[username] == password:
+        client_socket.send("truse".encode())
+
+        while True:
+            # Nhận lựa chọn từ client
+            choice = client_socket.recv(1024).decode()
+
+            if choice == "thoat":
+                break
+            elif choice == "datsan":
+                # Gửi danh sách sân trống về client
+                san_trong = get_san_trong()
+                data_to_send = {"san_trong": san_trong}
+                client_socket.send(json.dumps(data_to_send).encode())
+
+                # Nhận thông tin về sân và giờ đặt từ client
+                data_received = client_socket.recv(1024).decode()
+                data = json.loads(data_received)
+                chosen_san = data["chosen_san"]
+                chosen_gio_dat = data["chosen_gio_dat"]
+
+                # Kiểm tra xem sân đã đặt hay chưa
+                if chosen_san in san_trong:
+                    # Thêm vào tập hợp sân đã đặt
+                    san_da_dat.add((chosen_san, chosen_gio_dat))
+
+                    # Xử lý đặt sân
+                    result = f"Da dat san {chosen_san} vao luc {chosen_gio_dat}"
+                else:
+                    result = f"San {chosen_san} da bi dat, vui long chon san khac."
+
+                # Gửi kết quả đặt sân về client
+                client_socket.send(result.encode())
+            elif choice == "lichsu":
+                # Gửi danh sách sân đã đặt về client
+                san_da_dat_info = {"san_da_dat": list(san_da_dat)}
+                client_socket.send(json.dumps(san_da_dat_info).encode())
+            else:
+                client_socket.send("Invalid choice".encode())
+
+    else:
+        client_socket.send("Login failed".encode())
+
+    client_socket.close()
+
+def get_san_trong():
+    # Trả về danh sách sân còn trống (chưa được đặt)
+    san_trong = {"san1", "san2", "san3"} - set([san for san, _ in san_da_dat])
+    return ",".join(san_trong)
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("127.0.0.1", 8080))
+    server.listen(5)
+
+    print("[+] Listening for incoming connections")
+
+    while True:
+        client, addr = server.accept()
+        print("[+] Accepted connection from: %s:%d" % (addr[0], addr[1]))
+
+        handle_client(client)
+
+if __name__ == "__main__":
+    main()
